14873cabc0b6e3ca5ff145abd8f3d89345ffb92b
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-10-31 21:06:10 -0400

    Code to download, resize, upload.

--- resizer.py
+++ resizer.py
@@ -3,33 +3,57 @@
 
 from subprocess import check_output
 from sys import argv
+from time import time
 
-## install mogrify
+ts = time()
 
-aptget = 'apt-get install imagemagick'
-#check_output(aptget.split())
+## install needed things
+image = 'apt-get -y install imagemagick'
+cli = 'apt -y install awscli'
+check_output(image.split())
+check_output(cli.split())
 
 ## setup which jpgs to operate on
-
 myid = int(argv[1])
 maxid = int(argv[2])
-J = open('jpgs.txt').readlines()
+J = open('jpgs.txt').read().splitlines()
 n = len(J)
 n_per_worker = n / float(maxid)
 offset = (myid - 1) * n_per_worker
 
 start = int(round(offset))
 end = int(round(offset + n_per_worker))
-print(J[start:end])
-
-## download files from s3
+#print(J[start:end])
 
 
+## Operate on each file.
 
-## resize using mogrify & output new
+print('\nDownloading...')
+for f in J:
+    check_output(['curl', '-O',
+                  'https://s3.amazonaws.com/poland-card1/' + f])
 
+print('\nResizing...')
+t0 = time()
+for f in J:
+    fnew = f.replace('.JPG', '_t.JPG')
+    check_output(['convert', f, '-resize', '50%', fnew])
+t1 = time()
+print()
+print('Resize took', t1 - t0, 'sec =',
+      round((t1 - t0) / 60.0, 1), 'min.')
 
+print('\nUploading...')
+for f in J:
+    fnew = f.replace('.JPG', '_t.JPG')
+    S = 'aws s3 cp FILE s3://poland-card1/FILE'
+    S = S.replace('FILE', fnew)
+    check_output(S.split)
 
-## upload to s3
+te = time()
 
-
+print()
+print('Again, resize took', t1 - t0, 'sec =',
+      round((t1 - t0) / 60.0, 1), 'min.')
+print('Whole thing took', te - ts, 'sec =',
+      round((te - ts) / 60.0, 1), 'min.')
